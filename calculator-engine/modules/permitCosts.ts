b856852f5a9a202e4ea689343fee5f85adc8c05b
Philipp Doukakis <diukp2358@gmail.com>
2026-03-10 14:34:19 +0200

    Add permit & design fee module (KG700) and delete old modules

---
+++ calculator-engine/modules/permitCosts.ts
@@ -0,0 +1,32 @@
+import {
+  PERMIT_DESIGN_BASELINE_FEE,
+  PERMIT_DESIGN_BASELINE_AREA_MAX,
+  PERMIT_DESIGN_QUALITY_MULTIPLIERS
+} from "@/constants/construction"
+
+interface PermitCostsInput {
+  effectiveArea: number
+  qualityId: string
+}
+
+export function calculatePermitCosts(input: PermitCostsInput) {
+
+  let permitFee = PERMIT_DESIGN_BASELINE_FEE
+
+  if (input.effectiveArea > PERMIT_DESIGN_BASELINE_AREA_MAX) {
+
+    const extraArea =
+      input.effectiveArea - PERMIT_DESIGN_BASELINE_AREA_MAX
+
+    permitFee += extraArea * 20
+  }
+
+  const qualityMultiplier =
+    PERMIT_DESIGN_QUALITY_MULTIPLIERS[input.qualityId] ?? 1
+
+  permitFee *= qualityMultiplier
+
+  return {
+    permitFee
+  }
+}
