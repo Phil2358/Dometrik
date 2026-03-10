35f0c3e3dfda45193ec6bc133de8e0de317090de
Philipp Doukakis <diukp2358@gmail.com>
2026-03-10 14:25:11 +0200

    Add HVAC extras module

---
+++ calculator-engine/modules/hvacExtras.ts
@@ -0,0 +1,26 @@
+import { HVAC_OPTIONS } from "@/constants/construction"
+
+interface HvacExtrasInput {
+  effectiveArea: number
+  hvacSelections: Record<string, boolean>
+}
+
+export function calculateHvacExtras(input: HvacExtrasInput) {
+
+  let totalHvacCost = 0
+
+  for (const option of HVAC_OPTIONS) {
+
+    if (input.hvacSelections[option.id]) {
+
+      const optionCost =
+        input.effectiveArea * option.costPerSqm
+
+      totalHvacCost += optionCost
+    }
+  }
+
+  return {
+    hvacExtrasCost: totalHvacCost
+  }
+}
